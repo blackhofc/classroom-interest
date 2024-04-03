964b3ec57fa739713d535bbabef819ad16518d58
Benjamin <benjax48@hotmail.com.ar>
2024-04-03 07:52:00 -0300

    Feat animated student name.

--- src/components/Person.svelte
+++ src/components/Person.svelte
@@ -4,8 +4,8 @@
 </script>
 
 <div class="person-card">
-  <p class="simple-text">
-    Name: <span class="dynamic-text">{selectedNode.label}</span>
+  <p class="simple-text-animated">
+    {selectedNode.label}
   </p>
   <p class="simple-text">
     Age: <span class="dynamic-text">{selectedNode.age}</span>
@@ -37,6 +37,32 @@
     margin: 15px 15px 0; /* Top margin 20px, horizontal margins 20px, bottom margin 0 */
     padding: 10px 20px; /* Horizontal padding */
     border-radius: 4px;
+  }
+
+  .simple-text-animated {
+    margin: 4px 0;
+    font-size: 16px;
+    color: white;
+    font-weight: bold;
+    text-decoration: none;
+    background-image: linear-gradient(to right, #e52f89, #57bdba, #f5a800);
+    color: transparent;
+    background-size: 200% 100%;
+    -webkit-background-clip: text;
+    background-clip: text;
+    animation: animateGradient 10s linear infinite;
+  }
+
+  @keyframes animateGradient {
+    0% {
+      background-position: 200% 50%;
+    }
+    50% {
+      background-position: 0% 50%;
+    }
+    100% {
+      background-position: -200% 50%;
+    }
   }
 
   .simple-text {
